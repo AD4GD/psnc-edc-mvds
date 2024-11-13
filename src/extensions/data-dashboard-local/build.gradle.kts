1f432a463965182c1f5d8261aaf0fdbb5500ca9f
Daniel Parkhanovich <dparkhanovich@man.poznan.pl>
2024-11-13 16:29:14 +0100

    (DEM-320) Updated jupyter notebook version text. Removed wrong dependency in data-dashboard-local extension.

--- src/extensions/data-dashboard-local/build.gradle.kts
+++ src/extensions/data-dashboard-local/build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation(libs.edc.data.plane.selector.core)
     implementation(libs.edc.data.plane.selector.api)
 
-    implementation(libs.edc.transfer.data.plane)
     implementation(libs.edc.transfer.pull.http.receiver)
     implementation(libs.edc.http)
 
