adfc29c407ada3deed2d84f8dd65f902a12fae7e
mswiercz <mswiercz@man.poznan.pl>
2025-04-29 23:07:27 +0200

    remove unused src/other/ files - probably mix of unwanted files

--- src/other/main/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, url
-
-# all new endpoints that should be visible for end users must start with '/api'
-urlpatterns = [
-    url(r"^", include("common.urls")),
-]
