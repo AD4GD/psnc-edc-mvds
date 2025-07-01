b117fe2e9a7814e86061b8a64d89b41ae42e25f2
mswiercz <mswiercz@man.poznan.pl>
2025-07-01 19:30:27 +0200

    test if smoke tests upload to minio

--- docker/integration-tests/entrypoint.sh
+++ docker/integration-tests/entrypoint.sh
@@ -13,18 +13,35 @@ PROVIDER_MANAGEMENT="http://provider-connector:19193/management"
 CONSUMER_API="http://consumer-connector:29191/api"
 CONSUMER_MANAGEMENT="http://consumer-connector:29193/management"
 FEDERATED_CATALOG_URL="http://federated-catalog:8181/catalog"
+MINIO_ADDRESS="http://minio:9001/minio/webrpc"
 CONSUMER_BACKEND_EDR="http://consumer-backend:4000/edr-endpoint"
 PROVIDER_PROTOCOL_INTERNAL="http://provider-connector:19194/protocol"
+# PROVIDER_API="$LOCALHOST:19191/api"
+# PROVIDER_MANAGEMENT="$LOCALHOST:19193/management"
+# CONSUMER_API="$LOCALHOST:29191/api"
+# CONSUMER_MANAGEMENT="$LOCALHOST:29193/management"
+# FEDERATED_CATALOG_URL="$LOCALHOST:9181/catalog"
+# MINIO_ADDRESS="$LOCALHOST:9001/minio/webrpc"
+# DATA_SOURCE_PORT="5000"
+# MINIO_ACCESS_KEY="minio-user"
+# MINIO_SECRET_KEY="minio-password"
 
 # Default headers
 HEADERS="-H x-api-key:edc -H Content-Type:application/json"
 RETURN_CODE='--write-out %{http_code}\n -o /dev/null'
+RETURN_SIZE='--write-out %{size_download}\n -o /dev/null'
 
 # IDs
-UUID=$(cat /proc/sys/kernel/random/uuid)
+# UUID=$(cat /proc/sys/kernel/random/uuid)
+UUID=$(uuidgen)
 ASSET_ID="asset-$UUID"
 POLICY_ID="policy-$UUID"
 CONTRACT_DEF_ID="contract-definition-$UUID"
+# ASSET_ID="test-asset"
+# POLICY_ID="test-policy"
+# CONTRACT_DEF_ID="test-contract-definition"
+
+urls_to_download=("formats/csv" "formats/text" "formats/html" "formats/binary" "formats/file" "formats/stream")
 
 # 1. Conn Check
 log "=== Conn Check ==="
@@ -52,21 +69,32 @@ ASSET_PAYLOAD=$(jq -n \
 	--arg id "$ASSET_ID" \
 	--arg properties_name "$ASSET_ID" \
 	--arg properties_contenttype "application/json" \
+	--arg baseUrl "http://data-source:$DATA_SOURCE_PORT" \
 	'{
 		"@context" : { "edc" : "https://w3id.org/edc/v0.0.1/ns/" },
 		"@id" : $id,
 		"properties" : {
 			"name" : $properties_name,
-			"contenttype" : $properties_contenttype
+			"contenttype" : $properties_contenttype,
+			"proxyPath" : "true",
+			"proxyQueryParams" : "true",
+			"metadata" : { },
+			"baseUrl" : $baseUrl
 		},
 		"private_properties" : {
 			"name" : $properties_name,
-			"contenttype" : $properties_contenttype
+			"contenttype" : $properties_contenttype,
+			"proxyPath" : "true",
+			"proxyQueryParams" : "true",
+			"metadata" : { },
+			"baseUrl" : $baseUrl
 		},
 		"dataAddress" : {
 			"type" : "HttpData",
-			"name" : "Test data",
-			"baseUrl" : "https://jsonplaceholder.typicode.com/users"
+			"name" : $id,
+			"proxyPath" : "true",
+			"proxyQueryParams" : "true",
+			"baseUrl" : $baseUrl
 		},
 	}')
 ASSET_RES=$(curl -s -X POST $RETURN_CODE $HEADERS "$PROVIDER_MANAGEMENT/v3/assets" -d "$ASSET_PAYLOAD")
@@ -142,7 +170,7 @@ sleep 60
 
 # 5. Pobranie katalogu
 log "=== Fetch catalog ==="
-CATALOG=$(curl -L -X POST $HEADERS "$FEDERATED_CATALOG_URL/v1alpha/catalog/query")
+CATALOG=$(curl -sL -X POST $HEADERS "$FEDERATED_CATALOG_URL/v1alpha/catalog/query")
 if [ ${#CATALOG[@]} -eq 0 ]; then
     log "Error: Catalog is empty!"
 	exit 1
@@ -212,32 +240,125 @@ if [[ -z "$AGREEMENT_ID" ]]; then
 	exit 1
 fi
 
-# 9. Transfer asset
+# 9. Get MinIO credentials
+log "=== Get MinIO credentials ==="
+MINIO_LOGIN_PAYLOAD=$(jq -n \
+	--arg minio_user $MINIO_ACCESS_KEY \
+	--arg minio_password $MINIO_SECRET_KEY \
+	'{
+		"id": 1,
+		"jsonrpc": "2.0",
+		"method": "Web.Login",
+		"params": {
+			"username": $minio_user,
+			"password": $minio_password
+		}
+	}')
+MINIO_CREDENTIALS=$(curl -s \
+  -H "Content-Type: application/json" \
+  -H "User-Agent: Mozilla/5.0" \
+  $MINIO_ADDRESS -d "$MINIO_LOGIN_PAYLOAD")
+MINIO_TOKEN=$(echo $MINIO_CREDENTIALS | jq -r '.result.token')
+log "MINIO-TOKEN: $MINIO_TOKEN"
+
+# 10. Transfer asset
 log "=== Transfer asset ==="
-TRANSFER_PAYLOAD=$(jq -n \
-	--arg contract_agreement_id "$AGREEMENT_ID" \
-	--arg counter_party_address_internal "$PROVIDER_PROTOCOL_INTERNAL" \
-	--arg provider_connector_id "provider" \
-	--arg CONSUMER_BACKEND_EDR "$CONSUMER_BACKEND_EDR" \
-'{
-	"@context" : { "@vocab" : "https://w3id.org/edc/v0.0.1/ns/" },
-	"connectorId" : $provider_connector_id,
-	"counterPartyAddress" : $counter_party_address_internal,
-	"contractId" : $contract_agreement_id,
-	"assetId" : "assetId",
-	"protocol" : "dataspace-protocol-http",
-	"transferType" : "HttpData-PULL",
-	"dataDestination" : {
-		"type" : "HttpProxy",
-	},
-	"callbackAddresses": [{ "events" : ["transfer.process.started"], "uri" : $CONSUMER_BACKEND_EDR }],
-}')
-TRANSFER_RES=$(curl -s -X POST $RETURN_CODE $HEADERS "${CONSUMER_MANAGEMENT}/v2/transferprocesses" -d "$TRANSFER_PAYLOAD")
-if [[ $TRANSFER_RES -eq 200 ]] ; then
-	log "=== Transfer successfull ==="
-	log $TRANSFER_RES
-else
-	log "[ERROR] Unable to make a transfer: $TRANSFER_RES"
-	exit 1
-fi
+for url in "${urls_to_download[@]}"; do
+	TRANSFER_PAYLOAD=$(jq -n \
+		--arg contract_agreement_id "$AGREEMENT_ID" \
+		--arg counter_party_address_internal "$PROVIDER_PROTOCOL_INTERNAL" \
+		--arg provider_connector_id "provider" \
+		--arg CONSUMER_BACKEND_EDR "$CONSUMER_BACKEND_EDR/$url" \
+		--arg asset_id "$ASSET_ID" \
+	'{
+		"@context" : { "@vocab" : "https://w3id.org/edc/v0.0.1/ns/" },
+		"connectorId" : $provider_connector_id,
+		"counterPartyAddress" : $counter_party_address_internal,
+		"contractId" : $contract_agreement_id,
+		"assetId" : $asset_id,
+		"protocol" : "dataspace-protocol-http",
+		"transferType" : "HttpData-PULL",
+		"dataDestination" : {
+			"type" : "minio",
+		},
+		"callbackAddresses": [{ "events" : ["transfer.process.started"], "uri" : $CONSUMER_BACKEND_EDR }],
+	}')
+		
+	TRANSFER_RES=$(curl -s -X POST $HEADERS "${CONSUMER_MANAGEMENT}/v3/transferprocesses" -d "$TRANSFER_PAYLOAD")
+	TRANSFER_ID=$(echo "$TRANSFER_RES" | jq -r '.["@id"]')
+	if [[ -z $TRANSFER_ID ]] ; then
+		log "[ERROR] Unable to retrieve transfer ID!"
+		exit 1
+	else
+		log "TransferID: $TRANSFER_ID"
+	fi
+
+	y=1
+	while :;
+	do
+		TRANSFER_STATUS=$(curl -s $HEADERS "${CONSUMER_MANAGEMENT}/v3/transferprocesses/$TRANSFER_ID")
+		STATE=$(echo "$TRANSFER_STATUS" | jq -r '.state')
+		if [[ $y -gt 30 ]]; then
+			log "[ERROR] Could not start transfer process"
+			exit 1
+		elif [[ $STATE == "STARTED" ]]; then
+			break
+		else
+			y=$(( y + 1 ))
+			sleep 1
+			continue
+		fi
+	done
+	
+	RES=$(curl -s $HEADERS $RETURN_CODE "${CONSUMER_MANAGEMENT}/v3/transferprocesses/$TRANSFER_ID")
+	if [[ $RES -eq 200 ]]; then
+		log "=== Successfully downloaded $url ==="
+	else
+		log "[ERROR] Failed to download $url! Response code: $RES"
+		exit 1
+	fi
+done
+
+MINIO_BUCKET_NAME=$(curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "User-Agent: Mozilla/5.0" \
+  -H "Authorization: Bearer $MINIO_TOKEN" \
+  $MINIO_ADDRESS -d '{
+  	"id": 1,
+	"jsonrpc": "2.0",
+	"params": { },
+	"method": "Web.ListBuckets"
+  }' | jq -r '.result.buckets[0].name')
+echo "MINIO-BUCKET-NAME: $MINIO_BUCKET_NAME"
+
+MINIO_BUCKET_PAYLOAD=$(jq -n \
+	--arg bucket_name $MINIO_BUCKET_NAME \
+	'{
+		"id": 1,
+		"jsonrpc": "2.0",
+		"method": "Web.ListObjects",
+		"params": {
+			"bucketName": $bucket_name,
+			"prefix": "data/"
+		}
+	}')
+
+MINIO_ITEMS=$(curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "User-Agent: Mozilla/5.0" \
+  -H "Authorization: Bearer $MINIO_TOKEN" \
+  $MINIO_ADDRESS -d "$MINIO_BUCKET_PAYLOAD" | jq -r '.result.objects')
+
+MINIO_SIZES=($(echo "$MINIO_ITEMS" | jq '.[] | .size' | awk '{printf "%s ", $1}'))
+
+for index_url in "${!urls_to_download[@]}"; do
+	DS_SIZE=$(curl -s $RETURN_SIZE "$LOCALHOST:$DATA_SOURCE_PORT/${urls_to_download[$index_url]}")
+	if [[ $DS_SIZE -ne ${MINIO_SIZES[$index_url]} ]]; then
+		log "[ERROR] Invalid size for ${MINIO_SIZES[$index_url]}: ${MINIO_SIZES[$index_url]} vs $DS_SIZE"
+		exit 1
+	else
+		log "Valid size for ${MINIO_SIZES[$index_url]}: $DS_SIZE"
+	fi	
+done
+
 log "=== All tests finished successfully ==="
