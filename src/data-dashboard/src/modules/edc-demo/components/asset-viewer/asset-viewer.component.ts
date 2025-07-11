6b85a8c4d061dcb23191b13e3fc8e2a191d9b18f
mswiercz <mswiercz@man.poznan.pl>
2025-07-11 17:50:00 +0200

    pagination and improved search for assets

--- src/data-dashboard/src/modules/edc-demo/components/asset-viewer/asset-viewer.component.ts
+++ src/data-dashboard/src/modules/edc-demo/components/asset-viewer/asset-viewer.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { first, map, switchMap } from 'rxjs/operators';
+// import { BehaviorSubject, Observable, of } from 'rxjs';
+// import { first, map, switchMap, tap } from 'rxjs/operators';
+import { first } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { AssetInput, Asset } from "../../../mgmt-api-client/model";
 import { AssetService, QUERY_LIMIT } from "../../../mgmt-api-client";
 import { AssetEditorDialog } from "../asset-editor-dialog/asset-editor-dialog.component";
 import { ConfirmationDialogComponent, ConfirmDialogModel } from "../confirmation-dialog/confirmation-dialog.component";
 import { NotificationService, SorterService, UtilService } from "../../services";
+import { PageEvent } from '@angular/material/paginator';
 import { MetadataDisplayComponent } from '../common/metadata-display/metadata-display.component';
 import { METADATA_CONTEXT } from 'src/modules/app/variables';
 
@@ -18,10 +20,15 @@ import { METADATA_CONTEXT } from 'src/modules/app/variables';
 })
 export class AssetViewerComponent implements OnInit {
 
-  filteredAssets$: Observable<Asset[]> = of([]);
+  // filteredAssets$: Observable<Asset[]> = of([]);
+  // private fetch$ = new BehaviorSubject(null);
+  allAssets: Asset[] = [];
+  pagedAssets: Asset[] = [];
+  filteredAssets: Asset[] = [];
   searchText = '';
   isTransferring = false;
-  private fetch$ = new BehaviorSubject(null);
+  pageIndex = 0;
+  pageSize = 20;
 
   constructor(
     private assetService: AssetService,
@@ -33,32 +40,70 @@ export class AssetViewerComponent implements OnInit {
     public readonly utilService: UtilService,
   ) { }
 
-  private showError(error: string, errorMessage: string) {
-    this.notificationService.showError(errorMessage);
-    console.error(error);
+  loadAssets() {
+    this.assetService.requestAssets({ 
+      limit: QUERY_LIMIT,
+      offset: 0 
+    }).subscribe(assets => {
+      this.allAssets = assets.sort((a, b) =>
+        this.sorterService.naturalSort(
+          a.properties.optionalValue<string>('edc', 'name') || a['@id'],
+          b.properties.optionalValue<string>('edc', 'name') || b['@id'] 
+        )
+      );
+      this.applyFilterAndPagination();
+    });
   }
 
   ngOnInit(): void {
-    this.filteredAssets$ = this.fetch$
-      .pipe(
-        switchMap(() => {
-          const assets$ = this.assetService.requestAssets({
-            limit: QUERY_LIMIT,
-            offset: 0,
-          }).pipe(
-            map(assets => { 
-              console.log(assets); 
-              return assets.sort((a, b) => 
-                this.sorterService.naturalSort(a.properties.optionalValue<string>('edc', 'name') || '', b.properties.optionalValue<string>('edc', 'name') || ''))
-              })
-          );
-          return !!this.searchText
-            ? assets$.pipe(map(assets => assets.filter(asset => 
-              asset.properties.optionalValue<string>('edc', 'name')?.includes(this.searchText) 
-              || asset.id.toLowerCase().includes(this.searchText.toLowerCase())
-            )))
-            : assets$;
-        }));
+    this.loadAssets();
+    // this.filteredAssets$ = this.fetch$
+    //   .pipe(
+    //     switchMap(() => {
+    //       const assets$ = this.assetService.requestAssets({
+    //         limit: QUERY_LIMIT,
+    //         offset: 0,
+    //       }).pipe(
+    //         map(assets => { 
+    //           console.log(assets); 
+    //           return assets.sort((a, b) => 
+    //             this.sorterService.naturalSort(a.properties.optionalValue<string>('edc', 'name') || '', b.properties.optionalValue<string>('edc', 'name') || ''))
+    //           })
+    //       );
+    //       return !!this.searchText
+    //         ? assets$.pipe(map(assets => assets.filter(asset => 
+    //           asset.properties.optionalValue<string>('edc', 'name')?.includes(this.searchText)
+    //           || asset.id.toLowerCase().includes(this.searchText.toLowerCase())
+    //         )))
+    //         : assets$;
+    //     })
+    //   )
+    // this.filteredAssets$.subscribe(assets => {
+    //   this.pageIndex = 0; // reset przy zmianie filtrów
+    //   this.updatePagedAssets(assets);
+    // });
+  }
+
+  applyFilterAndPagination() {
+    // Filtrowanie
+    if (this.searchText) {
+      this.filteredAssets = this.allAssets.filter(asset =>
+        (asset.properties.optionalValue<string>('edc', 'name') || '').toLowerCase().includes(this.searchText.toLowerCase()) ||
+        (asset.properties.optionalValue<string>('edc', 'baseUrl') || '').toLowerCase().includes(this.searchText.toLowerCase()) ||
+        this.utilService.searchThroughMetadata(this.findMetadataForAsset(asset), this.searchText) ||
+        asset.id.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    } else {
+      this.filteredAssets = [...this.allAssets];
+    }
+    // Reset pageIndex jeśli poza zakresem
+    if (this.pageIndex * this.pageSize >= this.filteredAssets.length && this.filteredAssets.length > 0) {
+      this.pageIndex = 0;
+    }
+    // Paginacja
+    const start = this.pageIndex * this.pageSize;
+    const end = start + this.pageSize;
+    this.pagedAssets = this.filteredAssets.slice(start, end);
   }
 
   isBusy() {
@@ -66,11 +111,21 @@ export class AssetViewerComponent implements OnInit {
   }
 
   onSearch() {
-    this.fetch$.next(null);
+    this.pageIndex = 0;
+    // this.fetch$.next(null);
+    this.applyFilterAndPagination();
   }
 
-  isArray(val: any): val is any[] {
-    return Array.isArray(val);
+  onPageChange(event: PageEvent) {
+    if (event.pageSize !== this.pageSize) {
+      const firstItemIndex = this.pageIndex * this.pageSize;
+      this.pageIndex = Math.floor(firstItemIndex / event.pageSize);
+      this.pageSize = event.pageSize;
+    } else {
+      this.pageIndex = event.pageIndex;
+      this.pageSize = event.pageSize;
+    }
+    this.applyFilterAndPagination();
   }
 
   onDelete(asset: Asset) {
@@ -81,7 +136,8 @@ export class AssetViewerComponent implements OnInit {
       next: res => {
         if (res) {
           this.assetService.removeAsset(asset.id).subscribe({
-            next: () => this.fetch$.next(null),
+            next: () => this.loadAssets(),
+            // next: () => this.fetch$.next(null),
             error: err => this.showError(err, "This asset cannot be deleted"),
             complete: () => this.notificationService.showInfo("Successfully deleted")
           });
@@ -96,12 +152,17 @@ export class AssetViewerComponent implements OnInit {
       const newAsset = result?.assetInput;
       if (newAsset) {
         this.assetService.createAsset(newAsset).subscribe({
-          next: ()=> this.fetch$.next(null),
+          next: ()=> this.loadAssets(),
+          // next: () => this.fetch$.next(null),
           error: err => this.showError(err, "This asset cannot be created"),
           complete: () => this.notificationService.showInfo("Successfully created"),
         })
       }
     })
+  }
+  private showError(error: string, errorMessage: string) {
+    this.notificationService.showError(errorMessage);
+    console.error(error);
   }
 
   onSelect(asset: Asset) {
