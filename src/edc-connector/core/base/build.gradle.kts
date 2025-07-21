cb6423b532e9315d1ccb02b5a214f06589c151dd
Daniel Parkhanovich <dparkhanovich@man.poznan.pl>
2025-07-21 19:17:31 +0200

    (DEM-498) Move identity hub to edc-connector launchers, remove unused code

--- src/edc-connector/core/base/build.gradle.kts
+++ src/edc-connector/core/base/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation(libs.edc.api.observability)
 
     implementation(libs.psnc.auth.composite)
-    implementation(libs.psnc.vault.keys.seeder)
 
     // enable callbacks
     implementation(libs.edc.callback.event.dispatcher)
--- src/identity-hub/docs/developer/README.md
+++
@@ -1,5 +0,0 @@
-# Developer documentation
-
-* [Api](architecture/identityhub-apis.md)
-* [Api Security](architecture/identity-api.security.md)
-* [Decentralized Claims Protocol (DCP)](architecture/decentralized-claims-protocol/README.md)
