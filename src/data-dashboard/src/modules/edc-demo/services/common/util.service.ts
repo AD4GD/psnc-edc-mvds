970b0c30f6511feb4caf3519813f7124d775ef46
mswiercz <mswiercz@man.poznan.pl>
2025-07-22 23:47:35 +0200

    move filterAndPagination function to shared class

--- src/data-dashboard/src/modules/edc-demo/services/common/util.service.ts
+++ src/data-dashboard/src/modules/edc-demo/services/common/util.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from "@angular/core";
+import { PageEvent } from "@angular/material/paginator";
+import { filter } from "cypress/types/bluebird";
 
 @Injectable({
     providedIn: 'root'
@@ -36,5 +38,57 @@ export class UtilService {
         }
 
         return search(metadata);
+    }
+
+    onPageChange(
+        event: PageEvent, 
+        mainList: any[],
+        pageSize: number, 
+        filterFunction: Function, 
+        searchText: string = '', 
+        out: { filteredList: any[], pagedList: any[], pageIndex: number }
+    ) {
+        if (event.pageSize !== pageSize) {
+            const firstItemIndex = out.pageIndex * pageSize;
+            out.pageIndex = Math.floor(firstItemIndex / event.pageSize);
+            pageSize = event.pageSize;
+        } else {
+            out.pageIndex = event.pageIndex;
+            pageSize = event.pageSize;
+        }
+        this.applyFilterAndPagination(
+            mainList,
+            pageSize,
+            filterFunction,
+            searchText,
+            out
+        );
+    }
+
+    applyFilterAndPagination(
+        mainList: any[], 
+        pageSize: number, 
+        filterFunction: Function, 
+        searchText: string = '', 
+        out: { filteredList: any[], pagedList: any[], pageIndex: number }
+    ) {
+        // Filtering
+        let filteredList = [];
+        if (searchText !== '') {
+            filteredList = filterFunction([...mainList], searchText);
+        } else {
+            filteredList = [...mainList];
+        }
+        // Reset pageIndex if out of bands
+        if (out.pageIndex * pageSize >= filteredList.length && filteredList.length > 0) {
+            out.pageIndex = 0;
+        }
+        // Pagination
+        const start = out.pageIndex * pageSize;
+        const end = start + pageSize;
+        const pagedList = filteredList.slice(start, end);
+        // Update values through reference
+        out.filteredList = [...filteredList];
+        out.pagedList = [...pagedList];
     }
 }
