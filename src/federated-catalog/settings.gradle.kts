5ce09fe1bc6d471b944293db7a6efef84c8a8ea4
Daniel Parkhanovich <dparkhanovich@man.poznan.pl>
2025-08-06 18:50:43 +0200

    (DEM-498) Package changes for Federated Catalog and Identity Hub

--- src/federated-catalog/settings.gradle.kts
+++ src/federated-catalog/settings.gradle.kts
@@ -2,9 +2,13 @@ rootProject.name = "federated-catalog"
 
 include(":core:base")
 include(":launchers:daps")
+include(":launchers:dcp")
 include(":launchers:no-daps")
 include(":extensions:target-node-directory-sql")
 include(":extensions:catalog-node-static-resolver")
+include(":extensions:catalog-node-resolver")
+include(":extensions:did-example-resolver")
+include(":extensions:dcp-impl")
 
 // this is needed to have access to snapshot builds of plugins
 pluginManagement {
