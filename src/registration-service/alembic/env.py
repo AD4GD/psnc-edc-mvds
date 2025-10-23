d32793f826a23821a0ecd2967f2970cc5cb489f0
mswiercz <mswiercz@man.poznan.pl>
2025-10-23 17:13:11 +0200

    add registration_request to db

--- src/registration-service/alembic/env.py
+++ src/registration-service/alembic/env.py
@@ -1,15 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
+from alembic import context
+from api.core.database import Base
+from api.core.settings import PostgreSQLSettings
+from api.models.db import __all__  # noqa: F401
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-from alembic import context
-
-from api.core.settings import PostgreSQLSettings
-from api.core.database import Base
-from api.models.db import Location, Participant, IssuedCredentials, Contact
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
