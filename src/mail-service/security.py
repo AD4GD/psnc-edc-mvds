fb76aece66c3c23e10d12802fcf7aa8623d6de1b
mswiercz <mswiercz@man.poznan.pl>
2025-11-08 01:19:06 +0100

    run pre-commit

--- src/mail-service/security.py
+++ src/mail-service/security.py
@@ -1,9 +1,9 @@
-from fastapi import Header, HTTPException, Depends
-from settings import Settings
+from fastapi import Header, HTTPException
+from settings import Settings_
 
 
 async def verify_api_key(x_api_key: str = Header(None, alias="x-api-key")):
     """Dependency: Sprawdź API key z headera."""
-    if not x_api_key or x_api_key != Settings.x_api_key:
+    if not x_api_key or x_api_key != Settings_.x_api_key:
         raise HTTPException(status_code=403, detail="Invalid API Key")
     return x_api_key
--- src/mail-service/settings.py
+++ src/mail-service/settings.py
@@ -1,10 +1,12 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     x_api_key: str = Field(..., alias="X_API_KEY")
 
     class Config:
         env_file = ".env"
 
-Settings = Settings()
+
+_Settings = Settings()
