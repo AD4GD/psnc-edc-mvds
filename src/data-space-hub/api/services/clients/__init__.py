439f2a15818a2af576b89dd079dd4cd09836ca8f
mswiercz <mswiercz@man.poznan.pl>
2025-11-26 18:57:21 +0100

    pre-commit

--- src/data-space-hub/api/services/clients/__init__.py
+++ src/data-space-hub/api/services/clients/__init__.py
@@ -1,6 +1,5 @@
 from .email_service import EmailService
 from .keycloak_service import KeycloakService, keycloak_service
-from .node_connection_service import NodeConnectionService
 from .postgres_service import AsyncPostgresService, async_postgres_service
 from .vault_init import VaultInitializer
 from .vault_service import VaultService, vault_service
@@ -10,7 +9,6 @@ __all__ = [
     "KeycloakService",
     "VaultService",
     "AsyncPostgresService",
-    "NodeConnectionService",
     "VaultInitializer",
     "keycloak_service",
     "vault_service",
