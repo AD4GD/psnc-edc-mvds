53a31a70e5e892314d67d68b004ab126a61bc1a5
mswiercz <mswiercz@man.poznan.pl>
2026-02-01 01:05:03 +0100

    create extensions to fetch catalog using connector

---
+++ src/edc-connector/extensions/catalog-proxy/build.gradle.kts
@@ -0,0 +1,15 @@
+
+plugins {
+    `java-library`
+}
+
+dependencies {
+    implementation(libs.edc.core.spi)
+    implementation(libs.edc.control.plane.spi)
+    implementation(libs.edc.catalog.spi)
+    implementation(libs.edc.web.spi)
+    implementation(libs.jakarta.rsApi)
+    
+    // Using SPIs usually sufficient.
+    // If I need to send CatalogRequest, it might be in control-plane-spi or catalog-spi
+}
