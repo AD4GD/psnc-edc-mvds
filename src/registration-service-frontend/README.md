03ce7a312ed2ccdf082c86b65b1100d78611fb7b
Daniel Parkhanovich <dparkhanovich@man.poznan.pl>
2026-03-19 17:50:14 +0100

    DEM-585 Remove deprecated services

--- src/registration-service-frontend/README.md
+++
@@ -1,6 +0,0 @@
-# Registration Service Frontend
-
-This project contains a frontend implementation of the Registration Service written in Angular.
-
-## Run
-`npm start`
--- src/user-management-service/api/models/db/__init__.py
+++
@@ -1,7 +0,0 @@
-from .registration_request import RegistrationRequest
-from .registration_token import RegistrationToken
-
-__all__ = [
-    "RegistrationRequest", 
-    "RegistrationToken",
-]
